45b694c7be94311c6ea22f7cbc2f54bd74de67ec
Andrew Rosen <rosen@cs.gsu.edu>
2014-09-05 18:14:00 -0400

    Working on Euler22 for practice

---
+++ code/euler22.py
@@ -0,0 +1,38 @@
+names = open("p022_names.txt",'r').read()
+names = names.split(',')
+names  = map(lambda x: x.strip("\""), names) # clean out the pesky quotes
+
+
+def mergeSort(stuff):
+    size = len(stuff)
+    if size <=1 :
+        return stuff
+    # split into left and right
+    left = stuff[0:size/2]
+    right = stuff[size/2:size]
+    # divide
+    left = mergeSort(left)
+    right = mergeSort(right)
+
+    # conquer
+    return merge(left,right)
+
+
+def merge(a, b):
+    a.reverse()
+    b.reverse()
+    c = []
+    while len(a) > 0 and len(b) > 0:
+        if a[-1] < b[-1]:
+            c.append(a.pop())
+        else:
+            c.append(b.pop())
+    if len(a):
+        c.extend(a)
+    if len(b):
+        c.extend(b)
+    return c
+
+#test  =[5,3,7,1,6,7,30,21, 4523,23,7876,3142,2,4434,9]
+
+names = mergeSort(names)
