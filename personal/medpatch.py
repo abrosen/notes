f785058b5ba623eab2a343eae2bf8a48319ad5ee
Andrew Rosen <rosen@cs.gsu.edu>
2014-10-10 18:00:09 -0400

    Entered comments

---
+++ personal/medpatch.py
@@ -0,0 +1,8 @@
+## Program for extremely rough estimate of mythyl based on halflife of 2-4 hours
+## I am not a doctor, this is for my own personal use.
+## I am not liable for any use of this program by anyone else
+## see relvenent terms of the GNU 3
+
+
+
+#could model like a nicotine patch
