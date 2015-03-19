ddab5b5c6f7bc24a731f0c7e4754780fd2561c42
Andrew Rosen <rosen@cs.gsu.edu>
2015-03-19 17:53:22 -0400

    Proposal chapter done

--- personal/medpatch.py
+++ personal/medpatch.py
@@ -15,12 +15,12 @@ def decay(initial, t, halflife):
     return initial * (.5)**(t/halflife)
 
 
-def getLevels(initial, supplemental, halflife, interval,  num_sup=2, max_time=24):
+def getLevels(initial, supplementals, halflife, interval,  num_sup=2, max_time=24):
     times = np.arange(0,max_time,0.1)
     levels = [decay(initial, t, halflife) for t in times]
     for offset in xrange(1,num_sup+1):
         sup_levels =  np.zeros(interval*offset/0.1)
-        sup_levels =  list(sup_levels) + [decay(supplemental, t - interval*offset, halflife) for t in np.arange(interval*offset,max_time,0.1)]
+        sup_levels =  list(sup_levels) + [decay(supplementals[offset-1], t - interval*offset, halflife) for t in np.arange(interval*offset,max_time,0.1)]
         print sup_levels
         for i in xrange(len(levels)):
             levels[i] = levels[i]+sup_levels[i]
@@ -28,10 +28,10 @@ def getLevels(initial, supplemental, halflife, interval,  num_sup=2, max_time=24
 
     return times, levels
 
-times0, levels0 =  getLevels(10,10,2.1,3.5)
-times1, levels1 =  getLevels(10,10,2.5,3.5)
-times2, levels2 =  getLevels(10,10,2.9,3.5)
-times3, levels3 =  getLevels(10,10,3.3,3.5)
+times0, levels0 =  getLevels(10,[10,5],2.1,3.5)
+times1, levels1 =  getLevels(10,[10,5],2.5,3.5)
+times2, levels2 =  getLevels(10,[10,5],2.9,3.5)
+times3, levels3 =  getLevels(10,[10,5],3.3,3.5)
 
 plt.plot(times0,levels0, 'go-',label = "half-life = 2.1")
 plt.plot(times1,levels1, 'ko-',label = "half-life = 2.5")
