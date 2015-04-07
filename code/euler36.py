5ea27969004d11b8137060b4b76aaac73539000e
Andrew Rosen <rosen@cs.gsu.edu>
2015-04-07 16:03:40 -0400

    and python3

--- code/euler36.py
+++ code/euler36.py
@@ -2,10 +2,10 @@
 
 total = 0
 LIMIT = 1000000
-for i in xrange(LIMIT):
+for i in range(LIMIT):
     if str(i)[::-1] == str(i):
         if bin(i)[2:] == bin(i)[-1:1:-1]:
-            print i, bin(i)
+            print(i, bin(i))
             total += i
 
-print total
+print(total)
