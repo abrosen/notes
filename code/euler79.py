46bcea2f268ae111751254733a0605dfd42e9ae8
Andrew Rosen <rosen@cs.gsu.edu>
2015-04-13 17:13:44 -0400

    okay not done but I haave to got

--- code/euler79.py
+++ code/euler79.py
@@ -3,45 +3,72 @@
 FILE =  "p079_keylog.txt"
 
 class Node(object):
-    out = []
-    incoming = []
+    outs = []
+    ins = []
     def __init__(self, name):
         self.name = name
     
     def add_out(self, e):
-        self.out.append(e)
+        if e not in self.outs:
+            self.outs.append(e)
     
-    def add_incoming(self, e):
-        self.incoming.append(e)
+    def add_in(self, e):
+        if e not in self.ins:
+            self.ins.append(e)
 
-
-
-
-def doTheThing():
-    attempts = list(set(open(FILE, 'r').read().split()))
-    
-    print(attempts)
-    attempts  =  list(map(lambda x:  list(str(x)), attempts))
-    print(attempts)
-    
-    
+def createTheNodes(attempts):
     nums = []
     for attempt in attempts:
         for num in attempt:
             if num not in nums:
                 nums.append(num)
         
-    nodes = []
-    
+    nodes = {}
     for num in nums:
-        nodes.append(Node(num))
-    
+        nodes[num] = Node(num)
+
     for attempt in attempts:
-        pass
+        print(attempt)
+        a = attempt[0]
+        b = attempt[1]
+        c = attempt[2]
+        # add edge 1
+        nodes[a].add_out(b)
+        nodes[b].add_in(a)
+
+        # add edge 2
+        nodes[b].add_out(c)
+        nodes[c].add_in(b)
+    print(nodes[0].ins)
+    return nodes
+
+
+def doTheThing():
+    # stupid quick-to-write hack
+    # reading in the text file, each line (attempt) as an element
+    attempts = list(set(open(FILE, 'r').read().split()))
+    #now I split each attempt into a list of individual key presss
+    attempts  =  list(map(lambda x:  list(str(x)), attempts))
+   
     
-    
+    nodes = createTheNodes(attempts)
+
     ordering = []
-    S = set()
+    S = []
+    for n in nodes.values():
+        if len(n.ins) == 0:
+
+            S.append(n)
+    while(len(S)):
+        n =  S.pop()
+        ordering.append(n)
+        for m in n.outs:
+            print m
+            nodes[m].ins.remove(n.name)
+            if len(nodes[m].ins) == 0:
+                S.append(nodes[m])
+    print ordering
+
     
     
     
