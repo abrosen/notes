59e6979494553066f369aebed95072d20fd18eb6
Andrew Rosen <rosen@cs.gsu.edu>
2015-04-13 12:57:43 -0400

    I'll have to come back to this later!  Time for class

--- code/euler79.py
+++ code/euler79.py
@@ -1,1 +1,34 @@
+## This is a topological ordering problem
 
+FILE =  "p079_keylog.txt"
+
+class Node(object):
+    self.out = []
+    self.incoming = []
+    def __init__(self, name):
+        self.name = name
+    
+    def add_out(self, e):
+        self.out.append(e)
+    
+    def add_incoming(self, e):
+        self.incoming.append(e)
+
+
+def doTheThing():
+    attempts = list(set(open(FILE, 'r').read().split()))
+    
+    print(attempts)
+    attempts  =  list(map(lambda x:  list(str(x)), attempts))
+    print(attempts)
+    
+    
+    nodes = set()
+    ordering = []
+    S = set()
+    
+    
+    
+
+if __name__ == '__main__':
+    doTheThing()
