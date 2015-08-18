78df876175693cf3d3e3b6384a651f43afcd33e1
Andrew Rosen <rosen@cs.gsu.edu>
2015-08-18 15:02:27 -0400

    updated to python3

--- code/euler11.py
+++ code/euler11.py
@@ -3,7 +3,7 @@ def loadGrid(filename):
     numfile =  open(filename)
     for line in numfile:
         line = line.split()
-        line = map(int, line)
+        line =list(map(int, line))
         nums.append(line)
     return nums
 
@@ -13,8 +13,8 @@ def searchHorizontal(grid, size = 4):
     numCols = len(grid[0])
     numRows =  len(grid)
     best = -50
-    for i in xrange(numRows):
-        for j in xrange(numCols - size):
+    for i in range(numRows):
+        for j in range(numCols - size):
             product =  grid[i][j] * grid[i][j+1] * grid[i][j+2] * grid[i][j+3]
             if product > best:
                 best =  product
@@ -25,8 +25,8 @@ def searchVertical(grid, size = 4):
     numCols = len(grid[0])
     numRows =  len(grid)
     best = -50
-    for i in xrange(numRows - size):
-        for j in xrange(numCols):
+    for i in range(numRows - size):
+        for j in range(numCols):
             product =  grid[i][j] * grid[i+1][j] * grid[i+2][j] * grid[i+3][j]
             if product > best:
                 best =  product
@@ -37,8 +37,8 @@ def searchDiagonalUp(grid, size = 4):
     numCols = len(grid[0])
     numRows =  len(grid)
     best = -50
-    for i in xrange(size-1, numRows):
-        for j in xrange(numCols-size):
+    for i in range(size-1, numRows):
+        for j in range(numCols-size):
             product =  grid[i][j] * grid[i-1][j+1] * grid[i-2][j+2] * grid[i-3][j+3]
             if product > best:
                 best =  product
@@ -49,8 +49,8 @@ def searchDiagonalDown(grid, size = 4):
     numCols = len(grid[0])
     numRows =  len(grid)
     best = -50
-    for i in xrange(numRows-size):
-        for j in xrange(numCols-size):
+    for i in range(numRows-size):
+        for j in range(numCols-size):
             product =  grid[i][j] * grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3]
             if product > best:
                 best =  product
@@ -59,11 +59,11 @@ def searchDiagonalDown(grid, size = 4):
 
 grid = loadGrid("nums11")
 for row in grid:
-    print row
+    print(row)
 
-print  len(grid[0])
-print  len(grid)
-print searchHorizontal(grid)
-print searchVertical(grid)
-print searchDiagonalUp(grid)
-print searchDiagonalDown(grid)
+print(len(grid[0]))
+print(len(grid))
+print(searchHorizontal(grid))
+print(searchVertical(grid))
+print(searchDiagonalUp(grid))
+print(searchDiagonalDown(grid))
