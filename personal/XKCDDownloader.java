7ae25bdfb38a03a17a1a748a1e2516b40c023eb5
Andrew Rosen <retsej@gatech.edu>
2016-10-14 19:06:32 -0400

    Fixed

--- personal/XKCDDownloader.java
+++ personal/XKCDDownloader.java
@@ -1,12 +1,16 @@
+import java.nio.*;
 
 import java.net.*;
 import java.io.*;
+
 public class XKCDDownloader {
 	
 	
 	public static void main(String args[]) {
 		int currentComic =  22;
 		
+
+		String imgHeader= "http://imgs.xkcd.com/comics/";
 		try {
 			URL  url = new URL("http://xkcd.com/"+currentComic);
 			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -16,15 +20,25 @@ public class XKCDDownloader {
 				if(line == null ){
 					break;
 				}
-				System.out.print(line);
-			}
-			
+				if(line.contains(imgHeader)) {
+					int start = line.indexOf(imgHeader);
+					URL imgUrl = new URL(line.substring(start));
+					
+					//InputStream imgStream = imgURL.openStream();
+					Path out = new File("./"+currentComic).toPath();
+					//Files.copy(imgStream, out);
+
+					
+					System.out.println(start);
+					System.out.println(line.substring(start));
+
+				}
+			}	
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		
 	}
 
